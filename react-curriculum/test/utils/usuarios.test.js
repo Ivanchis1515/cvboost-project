3dae977eea3bb7719207a23be48b5d7172fa8666
AngelLuna6969 <utp0149900@alumno.utpuebla.edu.mx>
2024-08-12 21:19:55 -0600

    Actualizacion de los test de usuario

--- react-curriculum/test/utils/usuarios.test.js
+++ react-curriculum/test/utils/usuarios.test.js
@@ -1,6 +1,29 @@
+import { loginUser, registerUser } from "../../src/utils/RegisterLogin/RegisterLogin";
 import { checkAcceptance, acceptTerms, userDatainfo } from "../../src/utils/users/User";
 
+
 describe('Pruebas sobre las funciones de Usuarios', () => {
+
+    // test('Evaluando función que registra un usuario', async () => {
+    //     const correo = 'DavidG@gmail.com';
+    //     const contra = 'DavidGz23#';
+    //     const nombre = 'David Gonzales';
+    //     const usuario = (await registerUser(correo, contra, nombre)).data;
+    //     // Acceder a la propiedad 'msg'
+    //     const mensaje = usuario.msg;
+    //     const respuesta = "Usuario agregado"
+    //     //console.log(mensaje);
+    //     expect(mensaje).toEqual(respuesta);
+    // });
+
+    test('Evaluando función que valida si un usuario por ID acepto terminos', async () => {
+        const correo = 'DavidG@gmail.com';
+        const contra = 'DavidGz23#';
+        const usuario = await loginUser(correo,contra);
+        const respuesta = true
+        //console.log(usuario);
+        expect(usuario).toEqual(respuesta);
+    });
 
     test('Evaluando función que valida si un usuario por ID acepto terminos', async () => {
         const id = 1;
@@ -14,7 +37,7 @@ describe('Pruebas sobre las funciones de Usuarios', () => {
         const id = 1;
         const usuario = (await acceptTerms(id, 1)).data;
         const respuesta = { message: 'Terms accepted successfully' }
-        console.log(usuario);
+        //console.log(usuario);
         expect(usuario).toEqual(respuesta);
     });
 
