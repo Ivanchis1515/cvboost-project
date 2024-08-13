31c181c1ba52d8b8c37db4231a6db5f53dd2e272
AngelLuna6969 <utp0149900@alumno.utpuebla.edu.mx>
2024-08-12 20:54:22 -0600

    Test de usuarios

--- react-curriculum/test/utils/usuarios.test.js
+++ react-curriculum/test/utils/usuarios.test.js
@@ -1,17 +1,33 @@
-const { checkAcceptance } = require('../../src/utils/users/User');
+import { checkAcceptance, acceptTerms, userDatainfo } from "../../src/utils/users/User";
 
 describe('Pruebas sobre las funciones de Usuarios', () => {
-   
+
     test('Evaluando función que valida si un usuario por ID acepto terminos', async () => {
         const id = 1;
-        const usuario = checkAcceptance(id);
+        const usuario = await checkAcceptance(id);
+        const respuesta = true
+        //console.log(usuario);
+        expect(usuario).toEqual(respuesta);
+    });
+
+    test('Evaluando función que registra el concentimiento de los terminos a un usuario por ID', async () => {
+        const id = 1;
+        const usuario = (await acceptTerms(id, 1)).data;
+        const respuesta = { message: 'Terms accepted successfully' }
+        console.log(usuario);
+        expect(usuario).toEqual(respuesta);
+    });
+
+    test('Evaluando función que contiene la informacion de usuario por ID', async () => {
+        const id = 2;
+        const usuario = (await userDatainfo(id)).data;
         const respuesta = {
-            "status": "ok",
-            "msg": "Sí hay usuario",
-            "data": {
-                "id_usuario": 1
-            }
+            id: 2,
+            full_name: 'Jose Angel',
+            email: 'jals250808@gmail.com',
+            tipo: 'Usuario'
         }
-        expect(usuario).toBe(respuesta);
+        //console.log(usuario);
+        expect(usuario).toEqual(respuesta);
     });
 })
